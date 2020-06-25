78fccf69d7006a018ac338eac1e900bc9da9dd76
Erik Ford <ekford18@gmail.com>
2020-06-25 15:06:01 -0700

    completed film route testing

--- __tests__/film.test.js
+++ __tests__/film.test.js
@@ -7,8 +7,9 @@ const Film = require('../lib/models/Film');
 
 describe('Film Routes', async() => {
     
-  it('gets all films via GET', async() => {
+  it.only('gets all films via GET', async() => {
     const films = prepare(await Film.find().select({ title: true, released: true }).populate('studios.name'));
+    //ask TA about if .populate is working properly?
     console.log(films);
     return request(app)
       .get('/api/v1/films')
