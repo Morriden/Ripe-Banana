0ff0683cba3300929f82859d0c8d795ea713e9a4
Erik Ford <ekford18@gmail.com>
2020-06-25 13:53:04 -0700

    completed studio get all route

--- __tests__/studio.test.js
+++ __tests__/studio.test.js
@@ -1,19 +1,19 @@
-require('../lib/data-helper/data-helper');
+const { prepare } = require('../lib/data-helper/data-helper');
 
 const request = require('supertest');
 const app = require('../lib/app');
 
 const Studio = require('../lib/models/Studio');
 
-describe('Studio routes', () => {
+describe('Studio routes', async() => {
     
   it('gets all studios via GET', async() => {
+    const studios = prepare(await Studio.find().select({ name: true }));
+    
     return request(app)
       .get('/api/v1/studios')
       .then(res => {
-        expect(res.body).toEqual([{
-                
-        }]);
+        expect(res.body).toEqual(studios);
       });
   });
 
