456ea0c3a79d6489a663ff250925348742793d46
Max Lamb <maxlambcodes@gmail.com>
2020-06-25 18:01:09 -0700

    film path and test completed will work on post routes tomorrow

--- __tests__/film.test.js
+++ __tests__/film.test.js
@@ -5,7 +5,7 @@ const app = require('../lib/app');
 
 const Film = require('../lib/models/Film');
 
-describe('Film Routes', async() => {
+describe('Film Routes', () => {
     
   it('gets all films via GET', async() => {
     const films = prepare(await Film.find().select({ title: true, released: true }).populate('studio', { name: true }));
@@ -16,11 +16,16 @@ describe('Film Routes', async() => {
       });
   });
 
-  it('gets a film via get id', async() => {
+  it.only('gets a film via get id', async() => {
     const film = prepare(await Film
       .findOne()
       .populate('studio', { name: true })
       .populate('cast.actor', { name: true })
+      .populate({
+        path: 'review',
+        select: { rating: true, review: true, reviewer: true },
+        populate: { path: 'reviewer', select: { name: true } }
+      })
     );
     return request(app)
       .get(`/api/v1/films/${film._id}`)
