021f9aa78e7ef5ccd018b36a4915ba7d43c00e82
Erik Ford <ekford18@gmail.com>
2020-06-29 11:47:53 -0700

    application completed

--- __tests__/review.test.js
+++ __tests__/review.test.js
@@ -2,13 +2,13 @@ const { prepare } = require('../lib/data-helper/data-helper');
 
 const request = require('supertest');
 const app = require('../lib/app');
-const Reviewer = require('../lib/models/Reviewer');
+const Review = require('../lib/models/Review');
 const Film = require('../lib/models/Film');
 
 describe('Review routes', () => {
     
-  it.only('posts a review with the post route', async() => {
-    const reviewer = prepare(await Reviewer.findOne());
+  it('posts a review with the post route', async() => {
+    const reviewer = prepare(await Review.findOne());
     const film = prepare(await Film.findOne());
 
     return request(app)
@@ -29,6 +29,30 @@ describe('Review routes', () => {
           film: film.id,
           id: expect.anything()
         });
+      });
+  });
+
+  it('deletes a review', async() => {
+    const review = prepare(await Review.findOne());
+
+    return request(app)
+      .delete(`/api/v1/reviews/${review._id}`)
+      .then(res => {
+        expect(res.body).toEqual(review);
+      });
+  });
+
+  it('gets reviews with the get route, limited to 100 highest', async() => {
+    const reviews = prepare(await Review
+      .find()
+      .limit(100)
+      .sort({ 'rating': -1 })
+    );
+
+    return request(app)
+      .get('/api/v1/reviews')
+      .then(res => {
+        expect(res.body).toEqual(reviews);
       });
   });
 });
--- __tests__/reviewer.test.js
+++ __tests__/reviewer.test.js
@@ -4,6 +4,8 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 const Reviewer = require('../lib/models/Reviewer');
+const Review = require('../lib/models/Review');
+const Film = require('../lib/models/Film');
 
 describe('Reviewer routes', async() => {
 
@@ -41,6 +43,66 @@ describe('Reviewer routes', async() => {
           company: 'alchemy',
           __v: 0,
           id: expect.anything()
+        });
+      });
+  });
+
+  it('updates a reviewer with the patch route', async() => {
+    const reviewer = prepare(await Reviewer.findOne());
+
+    return request(app)
+      .patch(`/api/v1/reviewers/${reviewer._id}`)
+      .send({
+        name: 'Max',
+        company: 'Awesome'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.anything(),
+          name: 'Max',
+          company: 'Awesome',
+          __v: 0,
+          id: expect.anything()
+        });
+      });
+  });
+
+  it('deletes a user with no reviews', async() => {
+    const reviewer = await Reviewer.create({
+      name: 'Erik',
+      company: 'Alchemy',
+    });
+    return request(app)
+      .delete(`/api/v1/reviewers/${reviewer._id}`)
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.anything(),
+          name: 'Erik',
+          company: 'Alchemy',
+          __v: 0,
+          id: expect.anything()
+        });
+      });
+  });
+
+  it('It fails to delete a user because they have reviews', async() => {
+    const film = prepare(await Film.findOne());
+    
+    const reviewer = await Reviewer.create({
+      name: 'Erik',
+      company: 'Alchemy',
+    });
+    const review = await Review.create({
+      reviewer: reviewer._id,
+      film: film._id,
+      rating: 1,
+      review: 'Sucks badly'
+    });
+    return request(app)
+      .delete(`/api/v1/reviewers/${reviewer._id}`)
+      .then(res => {
+        expect(res.body).toEqual({
+          message: 'Reviewer Still Has Reviews', 'status': 500
         });
       });
   });
